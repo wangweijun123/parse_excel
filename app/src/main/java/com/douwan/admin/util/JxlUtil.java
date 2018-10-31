a429dea0f49f58b8b6600f2d3c4425f97466fa27
wangweijun <262448350@qq.com>
2018-10-31 13:48:48 +0800

    refactor

--- app/src/main/java/com/douwan/admin/util/JxlUtil.java
+++ app/src/main/java/com/douwan/admin/util/JxlUtil.java
@@ -1,57 +1,64 @@
 package com.douwan.admin.util;
 
+import android.os.Environment;
+import android.util.Log;
+
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import jxl.Cell;
+import jxl.CellType;
+import jxl.DateCell;
+import jxl.NumberCell;
 import jxl.Sheet;
 import jxl.Workbook;
 public class JxlUtil {
+	public static final String TAG = "wangweijun";
+	public static  void readXLS() {
+		try {
+			Workbook workbook = null;
+			try {
+				// 注意一定要xls的扩展名
+				File file=new File(Environment.getExternalStorageDirectory()+File.separator+"data2.xls");
+				workbook = Workbook.getWorkbook(file);
+//                InputStream inputStream= getAssets().open("data2.xls");
+//                workbook=Workbook.getWorkbook(inputStream);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return;
+			}
+			Sheet[] sheets = workbook.getSheets();
+			int size = sheets.length;
+			Log.i(TAG, "sheets size:"+size);
+			for (int i=0; i<size; i++) {
+				Sheet sheet = workbook.getSheet(i);
+				//列数
+				int columnCount = sheet.getColumns();
+				//行数
+				int rowCount = sheet.getRows();
+				//单元格
+				Log.i(TAG, "columnCount:"+columnCount+", rowCount:"+rowCount);
+				Cell cell = null;
+				Log.i(TAG, "第 " + i + "表 ..."+sheet.getName());
+				for (int everyRow = 0; everyRow < rowCount; everyRow++) {
+					Log.i(TAG, "第 " + everyRow + "行");
+					for (int everyColumn = 0; everyColumn < columnCount; everyColumn++) {
+						cell = sheet.getCell(everyColumn, everyRow);
+						if (cell.getType() == CellType.NUMBER) {
+							Log.i(TAG, "数字="+ ((NumberCell) cell).getValue());
+						} else if (cell.getType() == CellType.DATE) {
+							Log.i(TAG, "时间="+ ((DateCell) cell).getDate());
+						} else {
+							Log.i(TAG, "everyColumn="+everyColumn+",everyRow="+everyRow+
+									",cell.getContents()="+ cell.getContents());
+						}
+					}
+				}
+				Log.i(TAG, "第 " + i + "表 ... finished");
+			}
+			//关闭workbook,防止内存泄露
+			workbook.close();
+		} catch (Exception e) {
 
-	 public static List<String> xls2List(File file){
-		          List<String> result=new ArrayList<String>();
-		          try{
-		        	  
-		        	  //以路径读取
-		        	  //InputStream inp = new FileInputStream(path); 
-		        	  
-		        	  //以文件读取
-		             FileInputStream fis = new FileInputStream(file); 
-		              Workbook rwb = Workbook.getWorkbook(fis);
-		             Sheet[] sheet = rwb.getSheets();   
-		              for (int i = 0; i < sheet.length; i++) {   
-		                 Sheet rs = rwb.getSheet(i);   
-		                  for (int j = 0; j < rs.getRows(); j++) {   
-		                     Cell[] cells = rs.getRow(j);   
-		                    for(int k=0;k<cells.length;k++) {
-		                    	result.add(cells[k].getContents());
-		                    }
-		                 }   
-		              } 
-		              //路径读取
-		              //inp.close(); 
-		              
-		              //文件读取
-		              fis.close();
-		        }catch(Exception e){
-		              e.printStackTrace();
-		         }
-		         return result;
-		      }
-	 
-	 public static void main(String[] args){
-        
-		 //以路径读取
-          // List<String> result = xls2List("E:/111.xls");
-           
-		 //以文件读取
-           File file = new File("E:/111.xls");
-           List<String> result = xls2List(file);
-           for (int i = 0; i < result.size(); i++) {
-        	   System.out.println(result.get(i));
 		}
-      }
+	}
 }
--- app/src/main/java/com/example/administrator/parseexcel/MainActivity.java
+++ app/src/main/java/com/example/administrator/parseexcel/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.douwan.admin.util.JxlUtil;
+import com.douwan.admin.util.PoiUtil;
+
 import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.ss.usermodel.CellValue;
 import org.apache.poi.ss.usermodel.FormulaEvaluator;
@@ -16,7 +19,6 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
-import java.util.List;
 
 import jxl.Cell;
 import jxl.CellType;
@@ -40,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                readXLS();
+                JxlUtil.readXLS();
             }
         }).start();
     }
@@ -49,110 +51,14 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                readXlsx();
+                PoiUtil.readXlsx();
             }
         }).start();
     }
 
 
-    private void readXLS() {
-        try {
-            Workbook workbook = null;
-            try {
-                // 注意一定要xls的扩展名
-                File file=new File(Environment.getExternalStorageDirectory()+File.separator+"data2.xls");
-                workbook = Workbook.getWorkbook(file);
-//                InputStream inputStream= getAssets().open("data2.xls");
-//                workbook=Workbook.getWorkbook(inputStream);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return;
-            }
-            //得到第一张表
-            Sheet[] sheets = workbook.getSheets();
-            int size = sheets.length;
-            Log.i(tag, "sheets size:"+size);
-            for (int i=0; i<size; i++) {
-                Sheet sheet = workbook.getSheet(i);
-                //列数
-                int columnCount = sheet.getColumns();
-                //行数
-                int rowCount = sheet.getRows();
-                //单元格
-                Log.i(tag, "columnCount:"+columnCount+", rowCount:"+rowCount);
-                Cell cell = null;
-                for (int everyRow = 0; everyRow < rowCount; everyRow++) {
-                    for (int everyColumn = 0; everyColumn < columnCount; everyColumn++) {
-                        cell = sheet.getCell(everyColumn, everyRow);
-                        if (cell.getType() == CellType.NUMBER) {
-                            Log.i(tag, "数字="+ ((NumberCell) cell).getValue());
-                        } else if (cell.getType() == CellType.DATE) {
-                            Log.i(tag, "时间="+ ((DateCell) cell).getDate());
-                        } else {
-                            Log.i(tag, "everyColumn="+everyColumn+",everyRow="+everyRow+
-                                    ",cell.getContents()="+ cell.getContents());
-                        }
-                    }
-                }
-            }
 
-            //关闭workbook,防止内存泄露
-            workbook.close();
-        } catch (Exception e) {
 
-        }
-    }
 
-    public void readXlsx() {
-        InputStream stream = getResources().openRawResource(R.raw.data);
-        try {
-            XSSFWorkbook workbook = new XSSFWorkbook(stream);
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            int rowsCount = sheet.getPhysicalNumberOfRows();
-            Log.i("wangweijun", "rowsCount:"+rowsCount);
-            FormulaEvaluator formulaEvaluator = workbook.getCreationHelper().createFormulaEvaluator();
-            for (int r = 0; r<rowsCount; r++) {
-                Row row = sheet.getRow(r);
-                int cellsCount = row.getPhysicalNumberOfCells();
-                for (int c = 0; c<cellsCount; c++) {
-                    String value = getCellAsString(row, c, formulaEvaluator);
-                    String cellInfo = "r:"+r+"; c:"+c+"; v:"+value;
-                    Log.i("wangweijun", "value:"+value);
-                }
-            }
-        } catch (Exception e) {
-        }
-    }
-
-    protected String getCellAsString(Row row, int c, FormulaEvaluator formulaEvaluator) {
-        String value = "";
-        try {
-            org.apache.poi.ss.usermodel.Cell cell = row.getCell(c);
-            CellValue cellValue = formulaEvaluator.evaluate(cell);
-            switch (cellValue.getCellType()) {
-                case org.apache.poi.ss.usermodel.Cell.CELL_TYPE_BOOLEAN:
-                    value = ""+cellValue.getBooleanValue();
-                    break;
-                case org.apache.poi.ss.usermodel.Cell.CELL_TYPE_NUMERIC:
-                    double numericValue = cellValue.getNumberValue();
-                    if(HSSFDateUtil.isCellDateFormatted(cell)) {
-                        double date = cellValue.getNumberValue();
-                        SimpleDateFormat formatter =
-                                new SimpleDateFormat("dd/MM/yy");
-                        value = formatter.format(HSSFDateUtil.getJavaDate(date));
-                    } else {
-                        value = ""+numericValue;
-                    }
-                    break;
-                case org.apache.poi.ss.usermodel.Cell.CELL_TYPE_STRING:
-                    value = ""+cellValue.getStringValue();
-                    break;
-                default:
-            }
-        } catch (NullPointerException e) {
-            /* proper error handling should be here */
-        }
-        return value;
-    }
 
 }
